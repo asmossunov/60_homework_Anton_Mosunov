494843aea154ed7d39a1dc3200c51ba1c2706caa
Mosunov Anton <antonmossunov@gmail.com>
2022-09-30 12:58:28 +0600

    Регистрация модели в админ-панели и создание суперпользователя
    Настроена админ-панель
    Модель зарегистрирована в админ-панели
    Cоздан суперпользователь (root, root)

--- elmarket/market/admin.py
+++ elmarket/market/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from market.models import Product
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product_name', 'product_description', 'product_category', 'price', 'remains', 'changed_at', 'created_at')
+    list_filter = ('id', 'product_name', 'product_description', 'product_category', 'price', 'remains', 'changed_at', 'created_at')
+    search_fields = ('id', 'product_name', 'product_description', 'product_category', 'price', 'remains', 'changed_at', 'created_at')
+    fields = ('id', 'product_name', 'product_description', 'product_category', 'price', 'remains', 'changed_at', 'created_at')
+    readonly_fields = ['id']
+
+
+admin.site.register(Product, ProductAdmin)
