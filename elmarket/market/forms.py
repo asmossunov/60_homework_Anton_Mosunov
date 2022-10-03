650f6e28433ff54b87bdfc76869ba56009449e48
Mosunov Anton <antonmossunov@gmail.com>
2022-10-03 13:02:41 +0600

    Корректировка именования кнопки для удаления товара

--- elmarket/market/forms.py
+++ elmarket/market/forms.py
@@ -3,7 +3,7 @@ from django.core.exceptions import ValidationError
 from django.forms import widgets
 
 
-from market.models import CHOICES
+from market.models import CategoryChoices
 
 
 class ProductForm(forms.Form):
@@ -12,7 +12,7 @@ class ProductForm(forms.Form):
     product_description = forms.CharField(max_length=2000, required=True, label='Описание товара',
                                           widget=widgets.Textarea)
     product_image = forms.CharField(max_length=2000, required=True, label='Изображение товара')
-    product_category = forms.ChoiceField(choices=CHOICES, label='Категория товара')
+    product_category = forms.ChoiceField(choices=CategoryChoices.choices, label='Категория товара')
     price = forms.DecimalField(required=True, label='Стоимость товара')
     remains = forms.IntegerField(required=True, label='Остатки товара')
 
